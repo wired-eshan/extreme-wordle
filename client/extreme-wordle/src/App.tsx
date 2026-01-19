dc233ff624f6368adbb424d47bd786f326baec26
wired-eshan <eshanjan3@gmail.com>
2026-01-19 23:00:55 +0530

    Add a route for playground for websockets testing

--- client/extreme-wordle/src/App.tsx
+++ client/extreme-wordle/src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import './App.css'
-import Game from './components/Game'
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Game from "./components/Game";
+import Playground from "./Playground";
 
 function App() {
-
   return (
     <>
-      <div>
-        <Game/>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Game />} />
+          <Route path="/playground" element={<Playground />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
